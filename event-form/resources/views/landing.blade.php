bbe1682a196d4974f624bbbe7ddab6374f32c1ec
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-08-11 14:18:01 +0100

    fix grid

--- event-form/resources/views/landing.blade.php
+++ event-form/resources/views/landing.blade.php
@@ -23,13 +23,13 @@
             <div class="swiper-wrapper">
                 <!-- Item 1 -->
                 <div class="swiper-slide slide-center">
-                    <img data-aos="zoom-out-up" data-aos-delay="800" src="{{asset('_landing/assets/images/hero-11.png')}}" class="hero-image check-image" alt="Hero Image">
+                    <img data-aos="zoom-out-up" data-aos-delay="800" src="{{asset('_landing/assets/images/banner8.png')}}" class="hero-image check-image" alt="Hero Image">
                     <div class="slide-content row">
-                        <div class="col-12 d-flex inner">
-                            <div class="left align-self-center text-center text-md-left">
+                        <div class="col-12 d-flex inner justify-center">
+                            <div class=" col-12 align-self-center text-center ">
                                 <h1 data-aos="zoom-out-up" data-aos-delay="400" class="title effect-static-text">Digital Culture</h1>
                                 <p data-aos="zoom-out-up" data-aos-delay="800" class="description">Roadmap To Sustainable Performance Engine</p>
-                                <a href="#contact" data-aos="zoom-out-up" data-aos-delay="1200" class="smooth-anchor ml-auto mr-auto ml-md-0 mt-4 btn primary-button"><i class="icon-cup"></i>GET STARTED</a>
+                                <a href="#contact" data-aos="zoom-out-up" data-aos-delay="1200" class="smooth-anchor ml-auto mr-auto  mt-4 btn primary-button"><i class="icon-cup"></i>GET STARTED</a>
                             </div>
                         </div>
                     </div>
